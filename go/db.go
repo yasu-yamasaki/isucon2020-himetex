95ec9913213563c6f21c2e5f8f7e6095e467ba62
Takuya Oki <blutac.nz@gmail.com>
2020-09-12 15:23:05 +0900

    Update db.go

--- go/db.go
+++ go/db.go
@@ -24,7 +24,7 @@ type MySQLConnectionEnv struct {
 
 func NewMySQLConnectionEnv() (res MySQLConnectionEnv) {
 	res.withState = &MySQLConnectionEnvDetail{
-		Host:     "10.161.12.102",
+		Host:     "localhost",
 		Port:     "3306",
 		User:     "isucon",
 		DBName:   "isuumo",
