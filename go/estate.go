2bdb49ab7feca43d269d2cc7437b19cfbe4e53b6
t-oki <blutac.nz@gmail.com>
2020-09-12 16:36:22 +0900

    fix ドアに入るクエrい

--- go/estate.go
+++ go/estate.go
@@ -341,18 +341,18 @@ func searchRecommendedEstateWithChair(c echo.Context) error {
 	d := chair.Depth
 	var q1, q2 int64
 	if w > h {
-		if d > h {
-			q1, q2 = w, d
+		if w > d {
+			q1, q2 = h, d
 		}
 		q1, q2 = w, h
 	} else {
-		if w > d {
-			q1, q2 = w, h
+		if h > d {
+			q1, q2 = w, d
 		}
-		q1, q2 = h, d
+		q1, q2 = w, h
 	}
 	query = `SELECT * FROM estate WHERE (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) ORDER BY popularity DESC, id ASC LIMIT ?`
-	err = db.noState.SelectContext(ctx, &estates, query, q1, q2, q1, q2, Limit)
+	err = db.noState.SelectContext(ctx, &estates, query, q1, q2, q2, q1, Limit)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return c.JSON(http.StatusOK, EstateListResponse{[]Estate{}})
